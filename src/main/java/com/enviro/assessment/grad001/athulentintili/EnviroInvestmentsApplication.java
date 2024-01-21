411f15d9a4e4f58e9d31b4154d5f35cb35e43573
A2le <giftathule@gmail.com>
2024-01-21 13:51:08 +0200

    first commit

---
+++ src/main/java/com/enviro/assessment/grad001/athulentintili/EnviroInvestmentsApplication.java
@@ -0,0 +1,13 @@
+package com.enviro.assessment.grad001.athulentintili;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnviroInvestmentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnviroInvestmentsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/enviro/assessment/grad001/athulentintili/EnviroInvestmentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enviro.assessment.grad001.athulentintili;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnviroInvestmentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
